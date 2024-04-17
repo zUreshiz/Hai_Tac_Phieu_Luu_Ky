bdd40e16f2a4f6a306b5d67b34a20489a993a58a
HungDoHK <dkhanhhung58@gmail.com>
2024-04-17 17:36:40 +0700

    add floor, wall jump and timer

--- code/player.py
+++ code/player.py
@@ -1,4 +1,5 @@
 from settings import *
+from timer import Timer
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, collision_sprites):
@@ -12,11 +13,20 @@ class Player(pygame.sprite.Sprite):
 
         #movement
         self.direction = vector()
-        self.speed = 200
+        self.speed = 300
         self.gravity = 1000
+        self.jump = False
+        self.jump_height = 700
 
         #collision: va chạm
         self.collision_sprites = collision_sprites
+        self.on_surface = {'floor': False, 'left': False, 'right': False}
+
+        #Timer
+        self.timers = {
+            'wall jump': Timer(400),
+            'wall slide block': Timer(250)
+        }
 
     def input(self):
         '''Hàm xử lý sự kiện nhập từ bàn phím để điều khiển đối tượng di chuyển'''
@@ -24,12 +34,18 @@ class Player(pygame.sprite.Sprite):
         keys = pygame.key.get_pressed()
         # Tạo biến input_vector nếu right +1, left -1
         input_vector = vector(0,0)
-        if keys[pygame.K_RIGHT] or keys[pygame.K_d]: 
-            input_vector.x += 1
-        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            input_vector.x -= 1
-        # normalize để độ dài vector luôn là 1 
-        self.direction .x= input_vector.normalize().x if input_vector else input_vector.x
+        if not self.timers['wall jump'].active:
+            if keys[pygame.K_RIGHT] or keys[pygame.K_d]: 
+                input_vector.x += 1
+            if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+                input_vector.x -= 1
+            # normalize để độ dài vector luôn là 1 
+            self.direction .x= input_vector.normalize().x if input_vector else input_vector.x
+        
+
+        if keys[pygame.K_SPACE]:
+            self.jump = True
+
 
     def move(self, dt):
         '''tốc độ player khi di chuyển tung và hoành'''
@@ -38,12 +54,39 @@ class Player(pygame.sprite.Sprite):
         self.collision('horizontal')
 
         #vertical: tung
-        self.direction.y += self.gravity / 2 * dt
-        self.rect.y += self.direction.y * dt
-        self.direction.y += self.gravity / 2 * dt
+        if not self.on_surface['floor'] and any((self.on_surface['left'], self.on_surface['right'])) and not self.timers['wall slide block'].active:
+            self.direction.y = 0
+            self.rect.y += self.gravity / 10 * dt
+        else:
+            self.direction.y += self.gravity / 2 * dt
+            self.rect.y += self.direction.y * dt
+            self.direction.y += self.gravity / 2 * dt
+
+        if self.jump:
+            if self.on_surface['floor']:
+                self.direction.y = -self.jump_height
+                self.timers['wall slide block'].activate()
+            elif any((self.on_surface['left'], self.on_surface['right'])) and not self.timers['wall slide block'].active:
+                self.timers['wall jump'].activate()
+                self.direction.y = -self.jump_height
+                self.direction.x = 1 if self.on_surface['left'] else -1
+            self.jump = False
+
         self.collision('vertical')
 
 
+    def check_contact(self):
+        floor_rect = pygame.Rect(self.rect.bottomleft,(self.rect.width,2))
+        right_rect = pygame.Rect(self.rect.topright+ vector(0,self.rect.height / 4),(2, self.rect.height / 2))
+        left_rect = pygame.Rect(self.rect.topleft + vector(-2, self.rect.height / 4), (2, self.rect.height / 2))
+
+
+        collide_rects = [sprite.rect for sprite in self.collision_sprites]
+
+        #collision
+        self.on_surface['floor'] = True if floor_rect.collidelist(collide_rects) >= 0 else False
+        self.on_surface['right'] = True if right_rect.collidelist(collide_rects) >= 0 else False
+        self.on_surface['left'] = True if left_rect.collidelist(collide_rects) >= 0 else False
 
 
     def collision(self, axis):
@@ -64,7 +107,16 @@ class Player(pygame.sprite.Sprite):
                         self.rect.bottom = sprite.rect.top
                     self.direction.y = 0
 
+
+    def update_timers(self):
+        for timer in self.timers.values():
+            timer.update()
+
+
+
     def update(self, dt):
         self.old_rect = self.rect.copy()
+        self.update_timers()
         self.input()
         self.move(dt)
+        self.check_contact()
