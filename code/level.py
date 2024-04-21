ee6e8008c7798afe0fdfb96ea69464ebd45f73b6
HungDoHK <dkhanhhung58@gmail.com>
2024-04-21 11:04:23 +0700

    update new semi platform and add camera

--- code/level.py
+++ code/level.py
@@ -1,6 +1,7 @@
 from settings import *
 from sprites import Sprite, MovingSprite
 from player import Player
+from groups import AllSprite
 
 
 class Level:
@@ -8,8 +9,11 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
         #Groups
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprite()
         self.collision_sprites = pygame.sprite.Group()
+        self.semi_collision_sprites = pygame.sprite.Group()
+
+
         self.setup(tmx_map)
 
     def setup(self, tmx_map):
@@ -19,7 +23,7 @@ class Level:
 
         for obj in tmx_map.get_layer_by_name('Objects'):
             if obj.name =='player':
-                Player((obj.x,obj.y), self.all_sprites, self.collision_sprites)
+                self.player = Player((obj.x,obj.y), self.all_sprites, self.collision_sprites, self.semi_collision_sprites)
         
         #Moving Objects
         for obj in tmx_map.get_layer_by_name('Moving Objects'):
@@ -34,10 +38,10 @@ class Level:
                     start_pos = (obj.x + obj.width /2, obj.y)
                     end_pos = (obj.x + obj.width /2, obj.y + obj.height)     
                 speed = obj.properties['speed']
-                MovingSprite((self.all_sprites, self.collision_sprites), start_pos, end_pos, move_dir, speed)  
+                MovingSprite((self.all_sprites, self.semi_collision_sprites), start_pos, end_pos, move_dir, speed)  
 
 
     def run(self, dt):
         self.display_surface.fill('black')
         self.all_sprites.update(dt)
-        self.all_sprites.draw(self.display_surface)
+        self.all_sprites.draw(self.player.hitbox_rect.center)
